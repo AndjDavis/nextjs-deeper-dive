478de9e0f4d4a3e9c54d503563da60db00b6f262
AndjDavis <andi.idavis@gmail.com>
2024-09-06 16:37:30 -0600

    fixed styles in card component

--- app/_ui/card.tsx
+++ app/_ui/card.tsx
@@ -1,6 +1,6 @@
 const Card = ({ children }: { children: React.ReactNode }) => {
 	return (
-		<div className="flex justify-center items-center p-28 m-2.5 shadow-lg border-1 border-stone-800">
+		<div className="flex flex-col gap-3 bg-gray-50 justify-center items-center p-28 m-2.5 shadow-lg border-1 border-stone-800">
 			{children}
 		</div>
 	);
