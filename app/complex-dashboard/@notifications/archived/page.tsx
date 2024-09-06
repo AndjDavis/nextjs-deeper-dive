fc3dbceedecc416f2cb0b7f1f8b68819d11a0af1
AndjDavis <andi.idavis@gmail.com>
2024-09-06 16:38:00 -0600

    added archived route in notifications slot

--- app/complex-dashboard/@notifications/archived/page.tsx
+++ app/complex-dashboard/@notifications/archived/page.tsx
@@ -4,8 +4,8 @@ import Card from "@/app/_ui/card";
 export default function Page() {
 	return (
 		<Card>
-			<h1 className="font-bold text-xl">Notifications Slot</h1>
-			<Link href="/complex-dashboard/archived">Archived</Link>
+			<h1 className="font-bold text-xl">Archived Notifications Slot</h1>
+			<Link href="/complex-dashboard">Default Notifications</Link>
 		</Card>
 	);
 }
--- app/complex-dashboard/@revenue/page.tsx
+++ app/complex-dashboard/@revenue/page.tsx
@@ -3,7 +3,7 @@ import Card from "@/app/_ui/card";
 export default function Page() {
 	return (
 		<Card>
-			<h1 className="font-bold text-xl">Page Slot</h1>
+			<h1 className="font-bold text-xl">Revenue Slot</h1>
 		</Card>
 	);
 }
